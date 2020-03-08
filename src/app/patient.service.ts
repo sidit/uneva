367c56aed2e0a71cf12dfce3441062d2552829f1
sidit <siditnischal@gmail.com>
2020-03-08 18:07:36 +0530

    Added Patient's Model

--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -1,3 +1,4 @@
+import { Ipatient } from './patient';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -10,7 +11,7 @@ export class PatientService {
   private _url : string = "https://dev.uneva.in/task_721/list.php";
   constructor( private http : HttpClient ) { }
 
-  getPatient(): Observable<any>{
-    return this.http.get<any>(this._url);
+  getPatient(): Observable<Ipatient[]>{
+    return this.http.get<Ipatient[]>(this._url);
   }
 }
